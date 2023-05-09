02408973668ec8de9ece53a608ee41cfe31d6772
Shuvam <shanta.mitra.sm@gmail.com>
2023-05-10 02:55:10 +0530

    did css and other flask er jinis patro

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 # Define the path to the JSON file
 DATABASE = 'data.json'
+USERNAME = ''
 
 # Load existing data from the JSON file
 def load_data():
@@ -12,9 +13,9 @@ def load_data():
         with open(DATABASE, 'r') as file:
             data = json.load(file)
     except FileNotFoundError:
-        data = {"users": {},"tweets": []}
-        with open(DATABASE,'a')as f:
-            json.dump(data,f)
+        data = {"users": {}, "tweets": []}
+        with open(DATABASE, 'a')as f:
+            json.dump(data, f)
     return data
 
 # Save data to the JSON file
@@ -23,10 +24,20 @@ def save_data(data):
         json.dump(data, file, indent=4)
 
 # Home page - display all tweets
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     data = load_data()
-    return render_template('index.html', tweets=data['tweets'])
+
+    if request.method == 'POST':
+        tweet = request.form['tweet']
+        data['tweets'].append([USERNAME, tweet, data['users'][USERNAME][1]])
+        data['users'][USERNAME].append(tweet)
+        save_data(data)
+
+    if not USERNAME == '':
+        return render_template('index.html', tweets=data['tweets'][::-1], user=USERNAME, pfp=data['users'][USERNAME][1])
+    else:
+        return render_template('index.html', tweets=data['tweets'][::-1], user=USERNAME, pfp='')
 
 # User registration
 @app.route('/register', methods=['GET', 'POST'])
@@ -34,7 +45,7 @@ def register():
     if request.method == 'POST':
         data = load_data()
         username = request.form['username']
-        password=request.form['password']
+        password = request.form['password']
         if username in data['users']:
             error = 'Username already taken'
             return render_template('register.html', error=error)
@@ -48,29 +59,44 @@ def register():
 def login():
     if request.method == 'POST':
         data = load_data()
-        global username
-        username = request.form['username']
-        password=request.form['password']
-        if username not in data['users'] or data["users"][username][0]!=password:
+        global USERNAME
+        USERNAME = request.form['username']
+        password = request.form['password']
+        if USERNAME not in data['users'] or data["users"][USERNAME][0] != password:
             error = 'Invalid credentials'
             return render_template('login.html', error=error)
-        return redirect(url_for('profile'))
+        print(USERNAME)
+        return redirect(url_for('home'))
     return render_template('login.html')
 
 # User profile page - display user's tweets and allow posting new tweets
-@app.route('/profile', methods=['GET', 'POST'])
-def profile():
-   try: 
+@app.route('/account', methods=['GET', 'POST'])
+def account():
+    try:
         data = load_data()
-        user_tweets = data['users'][username][1:]
-        if request.method == 'POST':
-            tweet = request.form['tweet']
-            data['tweets'].append([username,tweet])
-            data['users'][username].append(tweet)
-            save_data(data)
-        return render_template('profile.html', username=username, tweets=user_tweets)
-   except:
-       return render_template('error.html')
+        user_tweets = data['tweets'][::-1]
+        # if request.method == 'POST':
+        #     tweet = request.form['tweet']
+        #     data['tweets'].append([USERNAME, tweet])
+        #     data['users'][USERNAME].append(tweet)
+        #     save_data(data)
+        return render_template('account.html', user=USERNAME, tweets=user_tweets)
+    except:
+        return render_template('error.html')
+
+@app.route('/profile/<user>', methods=['GET', 'POST'])
+def profile(user):
+    try:
+        data = load_data()
+        user_tweets = data['tweets'][::-1]
+        # if request.method == 'POST':
+        #     tweet = request.form['tweet']
+        #     data['tweets'].append([user, tweet])
+        #     data['users'][user].append(tweet)
+        #     save_data(data)
+        return render_template('profile.html', user=user , tweets=user_tweets)
+    except:
+        return render_template('error.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
